3d2326d674fe63deb90b92a6b54bf0c05cd64344
rotherprivat <60570144+rotherprivat@users.noreply.github.com>
2026-09-14 16:39:28 +0200

    Feature logging (#2)
    * option --log-file
    
    * removed AI generated resource file
    
    * Handle Copy errors
    
    * Small fix in german language resources
    
    * Readme
    
    * Grammer

--- Copy2AlbumFolder/AlbumGenerator.cs
+++ Copy2AlbumFolder/AlbumGenerator.cs
@@ -1,6 +1,8 @@
-﻿namespace Rotherprivat.Copy2AlbumFolder
+﻿using System.Security;
+
+namespace Rotherprivat.Copy2AlbumFolder
 {
-    internal class AlbumGenerator
+    internal sealed class AlbumGenerator : IDisposable
     {
         #region constructors
         internal AlbumGenerator() { }
@@ -22,7 +24,8 @@
 
             Out.Write(Resources.Copy2AlbumFolder.ConfirmationText,
                 SourceDirectory?.FullName,
-                AlbumDirectory?.FullName,
+                GetFsNameInfoString(AlbumDirectory),
+                GetFsNameInfoString(LogFile) ?? "---",
                 uiRecursive,
                 filenameExample,
                 Resources.Copy2AlbumFolder.ConfirmationPrompt
@@ -33,6 +36,12 @@
             return inputYes.Contains(input);
         }
 
+        internal static string? GetFsNameInfoString(FileSystemInfo? fsInfo) 
+        {
+            if (fsInfo == null) return null;
+            if (!fsInfo.Exists) return fsInfo.FullName + $" ({Resources.Copy2AlbumFolder.New})";
+            return fsInfo.FullName + $" ({Resources.Copy2AlbumFolder.Exists})";
+        }
 
         internal int Run()
         {
@@ -42,18 +51,49 @@
 
             if (!SourceDirectory.Exists) return -1;
 
+            // Open LogFile if specified
+            if (!ConditionallyCreateLogFile())
+                return -1;
+
             // Make sure AlbumDirectory exists
-            if (!AlbumDirectory.Exists)
-                AlbumDirectory.Create();
+            if (!ConditionallyCreateAlbumDirectory())
+                return -1;
 
             CopySourceFolder(SourceDirectory);
 
             return 0;
         }
 
-        #endregion
+        internal bool ConditionallyCreateAlbumDirectory()
+        {
+            if (AlbumDirectory == null)
+                throw new InvalidOperationException("AlbumDirectory can not be null");
 
-        #region private methodes
+            if (TryFileSystemAction(() => AlbumDirectory.Create()))
+                return true;
+
+            // Failed
+            Err?.WriteLine(Resources.Copy2AlbumFolder.ErrorCreateAlbumDirectory, AlbumDirectory.FullName);
+            return false;
+        }
+
+        internal bool ConditionallyCreateLogFile()
+        {
+            if (LogFile == null) return true;
+            if (_LogWriter != null) return true;
+            if (TryFileSystemAction(() =>
+            {
+                LogFile.Directory?.Create();
+                _LogWriter = new StreamWriter(LogFile.FullName, true);
+            })) return true;
+
+            // Failed
+            try { _LogWriter?.Dispose(); } catch { }
+            _LogWriter = null;
+            Err?.WriteLine(Resources.Copy2AlbumFolder.ErrorAccessLogFile, LogFile.FullName);
+            return false;
+        }
+
         internal void CopySourceFolder(DirectoryInfo directory)
         {
             if (Recursive)
@@ -79,18 +119,25 @@
                     var outputFile = GetUniqueOutputFilePath(file, dateTime!.Value.ToString(Pattern));
                     if (outputFile != null)
                     {
-                        File.Copy(file.FullName, outputFile);
-                        Out?.WriteLine($"{file.FullName}: {outputFile}");
+                        // Copy the file to the album directory
+                        if (TryFileSystemAction(() => File.Copy(file.FullName, outputFile)))
+                        {
+                            Log?.WriteLine($"{file.FullName}: {outputFile}");
+                        }
+                        else
+                        {
+                            Err?.WriteLine(Resources.Copy2AlbumFolder.ErrorCopyFile, file.FullName);
+                        }
                     }
                     break;
                 case Metadata.MetaDataResult.ErrorMissingDateTimeTag:
-                    Out?.WriteLine($"{file.FullName}: {Resources.Copy2AlbumFolder.ErrorMissingDateTimeTag}");
+                    Err?.WriteLine($"{file.FullName}: {Resources.Copy2AlbumFolder.ErrorMissingDateTimeTag}");
                     break;
                 case Metadata.MetaDataResult.ErrorProcessingMetaData:
-                    Out?.WriteLine($"{file.FullName}: {Resources.Copy2AlbumFolder.ErrorProcessingMetaData}");
+                    Err?.WriteLine($"{file.FullName}: {Resources.Copy2AlbumFolder.ErrorProcessingMetaData}");
                     break;
                 case Metadata.MetaDataResult.ErrorReadFile:
-                    Out?.WriteLine($"{file.FullName}: {Resources.Copy2AlbumFolder.ErrorReadFile}");
+                    Err?.WriteLine($"{file.FullName}: {Resources.Copy2AlbumFolder.ErrorReadFile}");
                     break;
                 default:
                     throw new InvalidOperationException("GetDateTimeFromMeta: Invalid result.");
@@ -117,9 +164,27 @@
         }
         #endregion
 
+        #region Private methods
+        private static bool TryFileSystemAction(Action action)
+        {
+            try
+            {
+                action();
+                return true;
+            }
+            catch (Exception e) when
+                (e is IOException ||
+                 e is SecurityException)
+            {
+                return false;
+            }
+        }
+        #endregion
+
         #region properties
         internal DirectoryInfo? SourceDirectory { get; set; }
         internal DirectoryInfo? AlbumDirectory { get; set; }
+        internal FileInfo? LogFile { get; set; }
         internal string? Pattern { get; set; }
         internal string? Postfix 
         {
@@ -131,10 +196,19 @@
         internal TextReader? In { get; set; }
         internal TextWriter? Out { get; set; }
         internal TextWriter? Err { get; set; }
-
-
+        internal TextWriter? Log => _LogWriter ?? Out;
         #endregion
 
         private string _Postfix = string.Empty;
+        private TextWriter? _LogWriter = null;
+
+        public void Dispose()
+        {
+            // Dispose any owned disposable resources
+            _LogWriter?.Dispose();
+            _LogWriter = null;
+
+            GC.SuppressFinalize(this);
+        }
     }
 }
